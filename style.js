a4521497126e6f29065a6b91631750151e0fb636
LeiraLeira <79096676+LeiraLeira@users.noreply.github.com>
2021-06-08 15:56:43 +0200

    Add initial cv website files

---
+++ style.js
@@ -0,0 +1,20 @@
+// function test() {
+//     var a = "3";
+//     var b = "8";
+//     a = b; // a = 8
+//     c = "3";
+//     b = c; // b = 3
+
+//     console.log("a is " + a);
+//     console.log("b is " + b);
+// }
+
+// test();
+
+var message = prompt("Hello");
+const total = 240;
+var rest = total - message;
+console.log("You have written " + message.length + " characters, " + "you have " + rest.length);
+;
+
+
